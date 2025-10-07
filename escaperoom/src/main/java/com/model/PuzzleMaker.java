4343eae04b7c7c8216ad83f9d0f17bfc6f3d16e6
CSE-JordanC <chavousn@email.sc.edu>
2025-10-07 15:51:22 -0400

    Issue #173 and #99

---
+++ escaperoom/src/main/java/com/model/PuzzleMaker.java
@@ -0,0 +1,11 @@
+package com.model;
+
+public class PuzzleMaker {
+
+    public PuzzleMaker() {
+    }
+
+    public Puzzle createPuzzle(String type, String question, String answer, Difficulty difficulty) {
+        return null;
+    }
+}
