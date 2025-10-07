3679382321692482d6c44e7183ae3c0fa05de9df
Tim Simpson <timothyms2006@gmail.com>
2025-10-07 13:40:23 -0400

    Issue #28

--- escaperoom/src/main/java/com/model/DataConstants.java
+++ escaperoom/src/main/java/com/model/DataConstants.java
@@ -1,16 +1,29 @@
 package com.model;
 
 public class DataConstants {
+    protected static final String USER_DATA_FILE = "escaperoom/src/main/java/com/data/users.json";
+    protected static final String ROOM_DATA_FILE = "escaperoom/src/main/java/com/data/rooms.json";
 
-    public static final String DATA_DIRECTORY = "JSON/";
+    //User JSON Keys
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_ID = "id";
+    public static final String KEY_CHARACTERS = "characters";
+    public static final String KEY_SETTINGS = "settings";
 
-    public static final String USER_DATA_FILE = DATA_DIRECTORY + "users.json";
-    public static final String ROOM_DATA_FILE = DATA_DIRECTORY + "rooms.json";
 
-    public static final String ERROR_LOADING_DATA = "Error loading data from file.";
-    public static final String ERROR_SAVING_DATA = "Error saving data to file.";
+    //Escape Room JSON Keys
+    protected static final String KEY_NAME = "roomName";
+    protected static final String KEY_DESCRIPTION = "description";
+    protected static final String KEY_IS_SOLVED = "isSolved";
+    protected static final String KEY_LEVEL = "level";
+
+    protected static final String ERROR_LOADING_DATA = "Error loading data from file.";
+    protected static final String ERROR_SAVING_DATA = "Error saving data to file.";
 
     private DataConstants() {
-        
+
     }
 }
+
+
