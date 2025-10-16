489db45a6ee4e9df7a34a2a490cd24da3620adb4
Tim Simpson <timothyms2006@gmail.com>
2025-10-16 14:02:48 -0400

    gamemanager, user, userloader

--- escaperoom/src/main/java/com/model/GameManager.java
+++ escaperoom/src/main/java/com/model/GameManager.java
@@ -2,24 +2,16 @@ package com.model;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.UUID;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
-/**
- * 
- * @author
- */
 public class GameManager {
     private User user;
     private Timer timer;
     private CharacterList characterList;
     private HintList hintList;
 
-    /**
-     * 
-     * @param username
-     * @param password
-     * @return
-     */
     public User login(String username, String password) {
         // find the user data in the JSON file matching the username 
         //check that the password is the same
@@ -29,27 +21,17 @@ public class GameManager {
     }
 
     /**
-<<<<<<< HEAD
-     * 
-     * @param username
-     * @param password
-     * @return
-=======
      * Creates a new Users when a new player signs up for the first time. The UUID is randomly generated.
      * @param username - the username this User selects
      * @param password - the password this User selects
      * @return - the User object created for this user.
->>>>>>> 377cb9732b672968dc17d60a67d52e5877576a8e
      */
     public User signup(String username, String password) {
         UUID id = UUID.randomUUID();
-        User user = new User(username, password, id);
-        addToJSON (user);
+        User user = new User(username, password);
+        return user;
     }
 
-    /**
-     * 
-     */
     public void playGame() {
         System.out.println("Game started.");
         if (timer == null) {
@@ -63,9 +45,6 @@ public class GameManager {
         }
     }
 
-    /**
-     * 
-     */
     public void pauseGame() {
         if (timer != null) {
             timer.cancel();
@@ -74,18 +53,6 @@ public class GameManager {
         }
     }
 
-<<<<<<< HEAD
-    /**
-     * 
-     */
-    public void exitGame() {
-    }
-
-    /**
-     * 
-     */
-    public void updateTime() {
-=======
     public void quitGame() {
         if (timer != null) {
             timer.cancel();
@@ -97,17 +64,5 @@ public class GameManager {
     public void updateTimeUI() {
         // Just a simple console message for now
         System.out.println("Updating time UI...");
->>>>>>> 377cb9732b672968dc17d60a67d52e5877576a8e
-    }
-
-    /**
-     * Adds the data for this User to JSON
-     * @param user the User object to add to the JSON file
-     */
-    private void addToJSON(User user){
-        try {
-            FileWriter writer = new FileWriter
-        } catch (Exception e) {
-        }
     }
 }
--- escaperoom/src/main/java/com/model/User.java
+++ escaperoom/src/main/java/com/model/User.java
@@ -1,207 +1,70 @@
-
 package com.model;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.UUID;
-
 /**
- * Represents a user with login credentials, a unique ID, and a list of characters.
+ * The User class represents a simple user with a username and email address.
+ * It provides methods for converting between User objects and a simple
+ * text-based representation.
+ *
  */
 public class User {
+    /** The username. */
     private String username;
-    private String password;
-    private UUID id;
-<<<<<<< HEAD
-    private ArrayList<Character> characterList;
-    private HashMap<String, Settings> settings;
-    private Progress progress;
+
+    /** The user's email address. */
+    private String email;
 
     /**
-     * Default Constructor that only requires the username, password and UUID. This would be used when a new user first 
-     * creates an account. The characterList is empty, the Hashmap for setting is empty and the progress object is initialized
-     * to default vaules.
-     * @param username - the string representing the username of the user
-     * @param password - a string password for this user (question - do we need to encrypt this?)
-     * @param id - the UUID for this user.
-     */
-=======
-    private ArrayList<Character> characters;
-
-    // Constructor
->>>>>>> 8d259037a6e192cc6408702797ade55f840f91c4
-    public User(String username, String password, UUID id) {
-        this.username = username;
-        this.password = password;
-        this.id = id;
-<<<<<<< HEAD
-        characterList = new ArrayList<Character>;
-        settings = new Hashmap();
-        progress = new Progress(0, new ArrayList<String>, 0, 0, username);
-
-        //do we write this to the JSON file when we create the user? I think we need to do this so that
-        //we can update the user info as they play the game.
-
-    }//end default constructor
-
-    /**
-     * This constructor is used by Leaderboard to retrieve existing users to display on the Leaderboard. Data is retrieved
-     * from the JSON file and provided to the constructor
-     * @param name = username for this User
-     * @param pwd = password for the User
-     * @param id = UUID for the User
-     * @param characters = ArrayList of Characters for this User
-     * @param settings = Hashmap of setting for this User
-     * @param prg = Progress object for this User.
-     */
-
-    public User(String name, String pwd, UUID id, ArrayList<Character> characters, HashMap settings, Progress prg) {
-        
-        username = name;
-        password = pwd;
-        this.id = id;
-        characterList = characters;
-        this.settings = settings;
-        progress = prg;
-        
-    }//end second constructor
-
-    /**
-     *  is there a  use case for another User constructor? Would we create a user AFTER they played a game and have a score?
-     * or would we just create the user when they first log on to play the game and then update fields as they progress 
-     * through the game?
-     * 
-     */
-
-    /**
-     * Adds a Character to the ArrayList of Characters for this User.
-     * @param character
-     */
-    public void addCharacter(Character character) {
-        characterList.add(character);
-    }//end addCharacter()
-
-    /**
-     * Deletes the specified character from the characterList for this user.
-     * @param character
-     */
-    public void deleteCharacter(Character character) {
-        characterList.remove (character);    
-    } //end deleteCharacter()
-
-    /**
-     * Updates the information in the Progress object for this User
-     * @param progress - a Progress object containing the updated information for this User
-     */
-    public void setProgress (Progress progress){
-        this.progress = progress;
-
-    } // end setProgress
-
-    /**
-     * Returns the current Progress Object for this User
-     * @return progress object for this User
-     */
-    public Progress getProgress (){
-        return progress;
-    } //end getProgress
-
-    /**
-     * Updates the settings for this User
-     * @param settings - Hashmap containing settings for this user
+     * Constructs a new User with the specified username and email.
      *
+     * @param username the user's username
+     * @param email the user's email address
      */
-    public void updateSettings (Hashmap settings){
-        this.settings  = settings;
-    } // end updateSettings
-
-    /**
-     * Returns the Hashmap containing the settings for this User
-     * @return settings - the Hashmap containing the settings for this User
-     */
-    public HashMap getSettings(){
-        return settings;
-    } // end getSettings
-
-    public String getName(){
-        return username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public UUID getID(){
-        return id;
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
     }
 
     /**
-     * Returns a string representation of this User and all of their data
-     * @return a string representation of this User and all of their data
+     * Returns the username of this user.
+     *
+     * @return the username as a String
      */
-    @Override
-    public String toString(){
-        String userData = ("\"username:\" " + username + " \"password:\" " + password +
-                " \"id:\" " + id + " \"characters:\" " + characterList + " \"Level:\" " + progress.getCurrentLevel() +
-                " \"puzzles:\" " + progress.getCompletedPuzzles() + " \"time spent:\" " + progress.getTimeSpent() + 
-                " \"score:\" " + progress.getScore());
-
-        System.out.println("User Data = " + userData);
-
-        return userData;
-    }
-}//end User
-=======
-        this.characters = new ArrayList<>();
-    }
-
-    // Add a character to the user
-    public void addCharacter(Character character) {
-        if (character != null) {
-            characters.add(character);
-        }
-    }
-
-    // Remove a character by name and avatar
-    public boolean deleteCharacter(String name, String avatar) {
-        Iterator<Character> iterator = characters.iterator();
-        while (iterator.hasNext()) {
-            Character character = iterator.next();
-            if (character.getName().equals(name) && character.getAvatar().equals(avatar)) {
-                iterator.remove();
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // Getters
     public String getUsername() {
         return username;
     }
 
-    public String getPassword() {
-        return password;
+    /**
+     * Returns the email address of this user.
+     *
+     * @return the email address as a String
+     */
+    public String getEmail() {
+        return email;
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public ArrayList<Character> getCharacters() {
-        return characters;
-    }
-
-    // Characters information output
+    /**
+     * Converts this User object into a single line of text
+     *
+     * @return the user data as a single String line
+     */
     @Override
     public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", id=" + id +
-                ", characterCount=" + characters.size() +
-                '}';
+        return username + "," + email;
+    }
+
+    /**
+     * Converts a line of text back into a User object.
+     *
+     * @param line a line of text representing a user
+     * @return a User object, or null if the line is invalid
+     */
+    public static User fromString(String line) {
+        String[] parts = line.split(",");
+        if (parts.length == 2) {
+            String username = parts[0].trim();
+            String email = parts[1].trim();
+            return new User(username, email);
+        }
+        return null;
     }
 }
-
->>>>>>> 8d259037a6e192cc6408702797ade55f840f91c4
