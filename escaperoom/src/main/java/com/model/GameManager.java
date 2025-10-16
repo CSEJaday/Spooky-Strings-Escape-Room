341f98ee7f930b5a780124142c683bf77b6ebacb
aklantz <aklantz@email.sc.edu>
2025-10-16 13:39:40 -0400

    updating

--- escaperoom/src/main/java/com/model/GameManager.java
+++ escaperoom/src/main/java/com/model/GameManager.java
@@ -2,29 +2,57 @@ package com.model;
 
 import java.util.*;
 
+/**
+ * 
+ * @author
+ */
 public class GameManager {
     private User user;
     private Timer timer;
     private CharacterList characterList;
     private HintList hintList;
 
+    /**
+     * 
+     * @param username
+     * @param password
+     * @return
+     */
     public User login(String username, String password) {
         return null;
     }
 
+    /**
+     * 
+     * @param username
+     * @param password
+     * @return
+     */
     public User signup(String username, String password) {
         return null;
     }
 
+    /**
+     * 
+     */
     public void playGame() {
     }
 
+    /**
+     * 
+     */
     public void pauseGame() {
     }
 
+    /**
+     * 
+     */
     public void exitGame() {
     }
 
+    /**
+     * 
+     */
     public void updateTime() {
     }
 }
--- escaperoom/src/main/java/com/model/ItemMaker.java
+++
@@ -1,8 +0,0 @@
-package com.model;
-
-public class ItemMaker {
-    public Item createItem(ItemName name)
-    {
-        return null;
-    }
-}
