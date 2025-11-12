d7e7ca659a262093896f431d481a1811ba856938
CSE-JordanC <chavousn@email.sc.edu>
2025-11-12 00:17:20 -0500

    home/login controller test

---
+++ escaperoom/src/main/java/com/example/LoginController.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    private void switchToHome() throws IOException {
+        App.setRoot("home");
+    }
+}
